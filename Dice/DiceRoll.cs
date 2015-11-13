44e3d329f4abff08a91464ad77d2f29392364589
Ryan Mainard <rmainard007@live.com>
2015-11-12 17:15:04 -0800

    inital source code commit

---
+++ Dice/DiceRoll.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    public class DiceRoll
+    {
+        Random _random = new Random();
+
+        public string roll(string diceNum)
+        {
+            StringBuilder rolledNum = new StringBuilder();
+            int numDice = Convert.ToInt32(diceNum);
+            for (int i = 0; i < numDice; i++)
+            {
+                rolledNum.Append(" " + _random.Next(1, 7));
+            }
+            return rolledNum.ToString();
+
+        }
+    }
+}
